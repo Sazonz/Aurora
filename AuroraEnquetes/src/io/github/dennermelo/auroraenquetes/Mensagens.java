d71fe407f3fe28ea7e674f7bc0b7ff8634b61db4
Denner G. Melo <dennergabrielmello@hotmail.com>
2019-03-12 00:27:42 -0300

    Create Mensagens.java

---
+++ AuroraEnquetes/src/io/github/dennermelo/auroraenquetes/Mensagens.java
@@ -0,0 +1,10 @@
+package io.github.dennermelo.auroraenquetes;
+
+public class Mensagens {
+	
+	
+	public static String Sem_Permissao;
+	public static String ID_Nao_Encontrado;
+	public static String Ja_Contem_Alternativa;
+
+}
